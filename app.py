da65891706f9de4e27b33059af04f46ba5218467
Rupesh Patel <68898106+Rupesh-git-eng@users.noreply.github.com>
2025-10-03 15:27:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,30 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn, addr = s.accept()
         with conn:
             count += 1
-            ## Time 
-            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            ## Time 
-            print(f"[{now}] [INFO] Connection #{count} from {addr}",flush=True)
-            conn.sendall(f"Connection count: {count}\n".encode())
+            #print(f"[+] Connection #{count} from {addr}")
+            print(f"[{now}] [INFO] Connection #{count} from {addr}",flush=True 
+            
+            body = f"Hello, World! 👋\nConnection count: {count}\n"
+            response = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n"
+                f"Content-Length: {len(body)}\r\n"
+                "Connection: close\r\n"
+                "\r\n"
+                f"{body}"
+            )
+            conn.sendall(response.encode())
+
+#with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#    s.bind((HOST, PORT))
+#    s.listen()
+#    print(f"[*] Listening on {HOST}:{PORT}")
+#    while True:
+#        conn, addr = s.accept()
+#        with conn:
+#            count += 1
+#            ## Time 
+#            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#            ## Time 
+#            print(f"[{now}] [INFO] Connection #{count} from {addr}",flush=True)
+#            conn.sendall(f"Connection count: {count}\n".encode())
