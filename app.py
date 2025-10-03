010ec1ba47aef5cb4a04e5018927a7723dfdb512
Rupesh Patel <68898106+Rupesh-git-eng@users.noreply.github.com>
2025-10-03 15:19:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import socket
 
+## Let's get time 
+
+import time
+import sys
+from datetime import datetime
+
+## Let's get time 
+
 HOST = "0.0.0.0"   # Listen on all interfaces
 PORT = 8080        # Port to listen on
 count = 0
@@ -12,5 +20,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn, addr = s.accept()
         with conn:
             count += 1
-            print(f"[INFO] Connection #{count} from {addr}",flush=True)
+            ## Time 
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            ## Time 
+            print(f"[{now}] [INFO] Connection #{count} from {addr}",flush=True)
             conn.sendall(f"Connection count: {count}\n".encode())
