4d87845a2c13de397e0a43972953c95e66447d8f
Rupesh Patel <68898106+Rupesh-git-eng@users.noreply.github.com>
2025-10-03 11:48:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = "0.0.0.0"   # Listen on all interfaces
+PORT = 8080        # Port to listen on
+count = 0
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"[*] Listening on {HOST}:{PORT}")
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            count += 1
+            print(f"[+] Connection #{count} from {addr}")
+            conn.sendall(f"Connection count: {count}\n".encode())
